ec00b3607c761ef0e627e09f7c18538c308d1bb2
bjsiu <bjsiu0617@gmail.com>
2016-08-22 14:47:35 +0800

    Add files via upload

--- gapps.sh
+++ gapps.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
 #
+# Copyright (C) 2016 RTAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
 # OpenGApps installation script for Raspberry Pi 3
 # Author: Igor Kalkov
-# https://github.com/RTAndroid/android_device_brcm_rpi3/blob/aosp-n/gapps.sh
+# https://github.com/RTAndroid/android_device_brcm_rpi3/blob/aosp-n/scripts/gapps.sh
 #
 
 package="open_gapps-arm-6.0-tvstock-20160816-UNOFFICIAL.zip"
@@ -44,25 +60,31 @@ echo "GApps installation script for RPi"
 echo "Used package: $package"
 echo "Device address: $address"
 
-if [ ! -d org ]; then
-  echo " * Downloading GApps package..."
+if [ ! -d "org" ]; then
+  echo " * Downloading OpenGApps package..."
   wget https://www.dropbox.com/s/cb2l9n5bs5g41fs/open_gapps-arm-6.0-tvstock-20160816-UNOFFICIAL.zip
   unzip $package -d org
 fi
 
+if [ ! -d "org" ]; then
+  echo "ERR: unzipping the package failed!"
+  exit 1
+fi
+
 echo " * Extracting supplied packages..."
-rm -rf tmp
+rm -rf tmp > /dev/null 2>&1
 mkdir -p tmp
 find . -name "*.tar.xz" -exec tar -xf {} -C tmp/ \;
 
-echo " * Removing not needed packages..."
-echo "  - SetupWizard (Tablet)"
-rm -rf tmp/setupwizardtablet*
+echo " * Removing conflicting packages..."
+echo "  - SetupWizard (TV)"
+rm -rf tmp/setupwraith* > /dev/null 2>&1
 echo "  - PackageInstaller (Google)"
-rm -rf tmp/packageinstallergoogle*
+rm -rf tmp/packageinstallergoogle* > /dev/null 2>&1
+rm -rf tmp/tvpackageinstallergoogle* > /dev/null 2>&1
 
 echo " * Creating system partition..."
-rm -rf sys
+rm -rf sys > /dev/null 2>&1
 mkdir -p sys
 for dir in tmp/*/
 do
@@ -88,15 +110,17 @@ echo " * Enforcing a reboot, please be patient..."
 wait_for_adb
 reboot_now
 
-echo " * Waiting for the device (errors are OK)..."
+echo " * Waiting for ADB (errors are OK)..."
 wait_for_adb
 
 echo " * Applying correct permissions..."
 adb shell "pm grant com.google.android.gms android.permission.ACCESS_COARSE_LOCATION"
 adb shell "pm grant com.google.android.gms android.permission.ACCESS_FINE_LOCATION"
-adb shell "pm grant com.google.android.setupwizard android.permission.READ_PHONE_STATE"
+adb shell "pm grant com.google.android.tungsten.setupwraith android.permission.READ_PHONE_STATE"
 
-echo "All done. The device will be rebooted once again."
+echo " * Waiting for ADB..."
 wait_for_adb
+
+echo "All done. The device will reboot once again."
 reboot_now
 adb kill-server
