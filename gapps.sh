96b91feff9fecb03b19d8106d79570e2b81d2718
bjsiu <bjsiu0617@gmail.com>
2016-08-26 12:53:53 +0800

    Update gapps.sh

--- gapps.sh
+++ gapps.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 
-#
-# Copyright (C) 2016 RTAndroid Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# raspberry pi android tv leanback gapps installtion script
 
-#
-# OpenGApps installation script for Raspberry Pi 3
-# Author: Igor Kalkov
-# https://github.com/RTAndroid/android_device_brcm_rpi3/blob/aosp-n/scripts/gapps.sh
-#
-
-package="open_gapps-arm-6.0-tvstock-20160816-UNOFFICIAL.zip"
+timestamp="20160816"
+package="open_gapps-arm-6.0-tvstock-$timestamp-UNOFFICIAL.zip"
 address="192.168.0.101"
 
 # ------------------------------------------------
@@ -62,7 +43,7 @@ echo "Device address: $address"
 
 if [ ! -d "org" ]; then
   echo " * Downloading OpenGApps package..."
-  wget https://www.dropbox.com/s/cb2l9n5bs5g41fs/open_gapps-arm-6.0-tvstock-20160816-UNOFFICIAL.zip
+  wget https://www.dropbox.com/s/cb2l9n5bs5g41fs/open_gapps-arm-6.0-tvstock-$timestamp-UNOFFICIAL.zip
   unzip $package -d org
 fi
 
